b958210cd4d1c3d930f020f5cc93cda9c02600d5
muskaan fayyaz <muskaanfayyaz3@gmail.com>
2024-03-16 00:56:17 +0500

    CLI calculator

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+    { message: "enter first number?", type: "number", name: "first" },
+    { message: "enter second number?", type: "number", name: "second" },
+    {
+        message: "enter operation?",
+        type: "list",
+        name: "operation",
+        choices: ["add", "sub", "mul", "div"]
+    }
+]);
+
+//conditional statement
+
+if (answers.operation === "add") {
+    console.log("your value after addition is", answers.first + answers.second);
+} else if (answers.operation === "sub") {
+    console.log("your value after subtraction is",answers.first - answers.second);
+} else if (answers.operation === "mul") {
+    console.log("your value after multiplication is",answers.first * answers.second);
+} else if (answers.operation === "div") {
+    console.log(" your value after division is",answers.first / answers.second);
+}
+else{
+    console.log("invalid operation");
+}
